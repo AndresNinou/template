67e6f08fe68e73dee62ba6093aeaf71a6a457ac7
mayor <ninoandres542@gmail.com>
2026-04-22 15:16:12 -0400

    Add TestC iteration 1 console log to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+console.log('TestC iteration 1')
+
 function App() {
   const [count, setCount] = useState(0)
 
